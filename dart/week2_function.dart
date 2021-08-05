94edf7f464923d5926b55bd99a81672720e1fb53
Nuttphum <beam_of_thai@hotmail.com>
2021-08-05 17:12:10 +0700

    Add Coment

--- dart/week2_function.dart
+++ dart/week2_function.dart
@@ -7,7 +7,7 @@ void main(){
  callWeb("Google", port: 443);
 }
 
-/* Normal
+/* Normal if elase แบบปกติ Codeยาวๆ
 void callGen(){
   var year = 1998;
 
@@ -20,22 +20,24 @@ void callGen(){
     print("Gen Y");
 
   */
-  //Arrow function  
+  //Arrow function  เพื่อช่วยลดบรรทัดในการเขียนแต่มีผลเหมือนกับCode if elase ด้านบน
+  //               เงื่อนไขถ้า year>= 1996   True ให้แสดง Geb Z false โปรแกรมจะแสดงว่าGen Y
   String callGen(int year) => (year >= 1996 ) ? ("gen Z") : ("gen Y") ;
   
  int callColor([String newColor = "White"]) {
 
     List<dynamic> color = ["red", "blue", "Black"];
+    //เป็นการสร้างfunction นับlengthใน color
     color.add(newColor);
     for (var item in color){
       print(item);
     } 
 
-    //print(color.length); รีเทรินจำนวนกลับไป
+    //print(color.length); รีเทรินจำนวนlenghtกลับไป
     return color.length;
 
   }
-  
+  //ในตัวแปร(String || int ) เราจำเป็นต้องกำหนดค่าเริ่มต้นให้ และถ้าที่Main ส่งparameter มาด้วยโปรแกรมจะเอา parameter ที่Mainส่งมาเลือกมาแสดง
   void callWeb(String web, {int port = 80, String protocal = "http" }) =>  
   print("$protocal $web  : $port");
 
